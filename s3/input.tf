c0707e935d43319d32c85c5a7e2a6a505c786c84
marysh <40528235+myantsevich@users.noreply.github.com>
2022-04-14 17:14:08 +0300

    Update input.tf

--- s3/input.tf
+++ s3/input.tf
@@ -1,15 +1,20 @@
 variable "bucket_name" {
   type = string
+  default = ""
 }
 variable "environment" {
   type = string
+  default = ""
 }
 variable "access_key" {
   type = string
+  default = ""
 }
 variable "secret_key" {
   type = string
+  default = ""
 }
 variable "region" {
   type = string
+  default = ""
 }
