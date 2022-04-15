5f897da77acd5432029ead74943a58c8e0502d08
marysh <40528235+myantsevich@users.noreply.github.com>
2022-04-15 15:48:34 +0300

    Update input.tf

--- s3/input.tf
+++ s3/input.tf
@@ -6,15 +6,3 @@ variable "environment" {
   type = string
   default = ""
 }
-variable "access_key" {
-  type = string
-  default = ""
-}
-variable "secret_key" {
-  type = string
-  default = ""
-}
-variable "region" {
-  type = string
-  default = ""
-}
